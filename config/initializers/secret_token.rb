e9b5e6b353748ee3dd4b6f49c38730f5f7e9de97
David Frey <dfrey1@binghamton.edu>
2012-11-14 23:32:34 -0500

    first coop commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coop::Application.config.secret_token = 'de89b815fef42222f51d2788206981954656b9069afa8addc35c856eb433e0d427894173f745a66027ba36f198a74f0bce7c38da37354b2f0a47c6969535086b'
