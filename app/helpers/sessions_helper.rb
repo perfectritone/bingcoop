7bf6dfef9bcdc33e8ac76fd079adfb1b47477515
David Frey <dfrey1@binghamton.edu>
2012-12-11 00:50:52 -0500

    remember token now sent a string

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token].to_s)
   end
   
   def current_user?(user)
