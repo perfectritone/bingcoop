d9813ef74b7f155503c0d56533387c954bf8d986
David Frey <dfrey1@binghamton.edu>
2012-12-31 07:08:06 -0500

    markup validations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,6 @@ Coop::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
-  # Connect to the database during asset precompiling
-  config.assets.initialize_on_precompile = false
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
