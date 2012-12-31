06c0e8a0437458d3b8519e155b1d45471871bea0
David Frey <dfrey1@binghamton.edu>
2012-12-31 06:23:33 -0500

    markup validation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
   
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", class: "remove_ingredient") + "</input>"
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
   
   def link_to_add_fields(name, f, association)
