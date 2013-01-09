edf7eaf50b323f244ff73f399309cbe15d6a387c
David Frey <dfrey1@binghamton.edu>
2013-01-08 20:29:51 -0500

    update for heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,15 +26,15 @@ function add_fields(link, association, content) {
 }
 
 function equalize_side_box_heights() {
-  left_box_height = $('#left_box').height();
-  right_box_height = $('#right_box').height();
+  left_box_height = $('#left_box .top_box').height();
+  right_box_height = $('#right_box .top_box').height();
   if ( parseInt(left_box_height) == 0 || parseInt(right_box_height) == 0 )
     return;
   if (left_box_height < right_box_height) {
-    $('#left_box').height(right_box_height);
+    $('#left_box .top_box').height(right_box_height);
   } else {
-    $('#right_box').height("100");
+    $('#right_box .top_box').height(left_box_height);
   }
 }
 
-$(equalize_side_box_heights);
+$(document).ready(equalize_side_box_heights);
